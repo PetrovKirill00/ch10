ed158f700f1cdc273b4273fba37dff9e5c55c73c
Kirill Petrov <kosyanip@gmail.com>
2026-05-29 11:47:25 +0300

    second commit

--- ch10.go
+++ ch10.go
@@ -1,5 +1,11 @@
+// A simple library for adding two numbers
 package ch10
 
+// Add adds two numbers together
+//
+// More info on additon at [MathIsFun].
+//
+// [MathIsFun]: https://www.mathsisfun.com/numbers/addition.html
 func Add(a, b int) int {
 	return a + b
 }
